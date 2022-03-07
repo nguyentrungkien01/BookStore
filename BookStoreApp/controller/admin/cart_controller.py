6762abe6636e4ce4af55a8039279ded1f92979c5
vantruong2220 <1951052220truong@gmail.com>
2022-03-07 23:32:08 +0700

    update

--- BookStoreApp/controller/admin/cart_controller.py
+++ BookStoreApp/controller/admin/cart_controller.py
@@ -5,8 +5,7 @@ from BookStoreApp.service.admin.cart_service import get_info_user_in_cart as inf
 
 @app.route("/admin/api/cart", methods=['post'])
 def get_cart():
-    print("00000000000000000000000000000000000000000000000000-")
-    print(gbic())
-    print(info_user())
+    # print(gbic())
+    # print(info_user())
     # return render_template("/admin/cart.html", info_user=info_user(), list_book =gbic())
     return jsonify(gbic())
