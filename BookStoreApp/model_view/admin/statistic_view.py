d554ec5f2498302b351d6d66c97731994365f46f
= <=>
2022-03-08 21:52:36 +0700

    statistic - feature (admin)

--- BookStoreApp/model_view/admin/statistic_view.py
+++ BookStoreApp/model_view/admin/statistic_view.py
@@ -2,13 +2,15 @@ from flask_admin import expose
 from flask_login import current_user
 
 from BookStoreApp.model_view.admin.base_view import BaseView
+from BookStoreApp.controller.utils.utils_controller import get_data_json_file as gdjf
 
 
 # Lớp tượng trưng cho trang chức năng thống kê phía admin
 class StatisticView(BaseView):
     @expose('/')
     def index(self):
-        return self.render('/admin/statistic.html')
+        selections = gdjf('statistic_data.json')
+        return self.render('/admin/statistic.html', selections=selections)
 
     def is_accessible(self):
         return current_user.is_authenticated
