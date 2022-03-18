42aed30a26cda5f1d6d4f43a8b93c045fe276209
VanTruong <71361029+nguyenvantruong-ou@users.noreply.github.com>
2022-03-18 10:16:42 +0700

    Features/info user (#37)
    * profile in admin
    
    * profile in admin
    
    * update profile in admin
    
    * .
    
    * fix conflict
    
    * update profile 13/03/2022
    
    * fix conflict
    
    * signup in client
    
    * update signup
    
    * sign in
    
    * account-info
    
    Co-authored-by: vantruong2220 <1951052220truong@gmail.com>

--- BookStoreApp/static/js/non_admin/sign_in.js
+++ BookStoreApp/static/js/non_admin/sign_in.js
@@ -1,5 +1,7 @@
-$('#btnSignIn').click(function () {
-    if (isExactlyDataSignIn() == true) {
+var url   //update 17/03/2022
+$('#btnSignIn').click(function (){
+    url = location.href // update 17/03/2022
+    if(isExactlyDataSignIn() == true){
         fetch('/client/api/sign-in', {
             method: 'post',
             body: JSON.stringify({
@@ -10,31 +12,36 @@ $('#btnSignIn').click(function () {
                 'Accept': 'application/json',
                 'Context-Type': 'application/json',
             }
-        }).then(res => res.json()).then(data => {
-            if (data == 'error') {
-                Swal.fire({
-                    title: 'Tên đăng nhập hoặc mật khẩu sai !',
-                    text: 'Xin vui lòng kiểm tra lại',
-                    icon: 'warning',
-                    confirmButtonColor: '#3085d6',
-                    confirmButtonText: 'Ok',
-                })
-            } else
-                window.location = "/"
+
+            }).then(res => res.json()).then(data => {
+                if(data == 'error'){
+                    Swal.fire({
+                        title: 'Tên đăng nhập hoặc mật khẩu sai !',
+                        text: 'Xin vui lòng kiểm tra lại',
+                        icon: 'warning',
+                        confirmButtonColor: '#3085d6',
+                        confirmButtonText: 'Ok',
+                    })
+                } else
+                    window.location=url  // update 17/03/2022
+//                    window.location="/tai-khoan"
+
         })
     }
 })
 
 // kiểm tra thông tin nhập
-function isExactlyDataSignIn() {
-    if (document.getElementById('usernameSignIn').value == '' || document.getElementById('passwordSignIn').value == '') {
+function isExactlyDataSignIn(){
+    if (document.getElementById('usernameSignIn').value == '' || document.getElementById('passwordSignIn').value == ''){
+
         Swal.fire({
             title: 'Vui lòng kiểm tra lại !!!',
             text: '',
             icon: 'warning',
             confirmButtonColor: '#3085d6',
             confirmButtonText: 'Ok',
-        })
+            })
+
         return false
     }
     return true
