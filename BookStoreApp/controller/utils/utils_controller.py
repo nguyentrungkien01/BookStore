9143e960ffd28950739a42a974ba2b21eed4dfcd
Nguyễn Trung Kiên <93910078+nguyentrungkien01@users.noreply.github.com>
2022-04-04 10:24:46 +0700

    Update utils_controller.py

--- BookStoreApp/controller/utils/utils_controller.py
+++ BookStoreApp/controller/utils/utils_controller.py
@@ -24,7 +24,7 @@ def set_data_json_file(filename, data):
 
 # Mã hóa bằng thuật toán vigenere
 def encode_vigenere(plain_text):
-    # Lấy số nhỏ nhất và lớn nhất có 26 chữ số
+    # Lấy số nhỏ nhất và lớn nhất có 10 chữ số
     beg_num = '1'
     end_num = ''
     for x in range(1, 11):
@@ -36,10 +36,18 @@ def encode_vigenere(plain_text):
     secret_number = random.randint(int(beg_num) - plain_text, int(end_num) - plain_text)
     secret_data = str(plain_text + secret_number)
 
+    # data source
+    data_source = []
+    for dt in range(65, 91):
+        data_source.append(chr(dt))
+    for dt in range(97, 123):
+        data_source.append(chr(dt))
+
     # tạo key
     first_key = ''
     second_key = ''
-    key_source = [x for x in range(0, 10)]
+    key_index = random.randint(0, len(data_source) - 10)
+    key_source = [x for x in data_source[key_index:key_index + 10]]
     random.shuffle(key_source)
     for key in key_source:
         first_key += str(key)
@@ -47,25 +55,40 @@ def encode_vigenere(plain_text):
     for key in key_source:
         second_key += str(key)
 
+    # Tạo map pivot
+    map_pivot = [x for x in first_key]
+    for dt in data_source:
+        if not map_pivot.__contains__(dt):
+            map_pivot.append(dt)
+
     # Tạo bảng ánh xạ
     map_table = []
-    map_pivot = [int(x) for x in first_key]
-    for r in range(0, 10):
-        map_table.append([map_pivot[c] for c in range(0, 10)])
-        map_pivot.append(map_pivot.pop(0))
+    map_pivot_temp = [x for x in map_pivot]
+    for r in range(0, len(data_source)):
+        map_table.append([map_pivot_temp[c] for c in range(0, len(data_source))])
+        map_pivot_temp.append(map_pivot_temp.pop(0))
 
     # Ánh xạ và tiến hành mã hóa
     pivot = 0
     cipher_data = ''
     for sd in secret_data:
-        cipher_data += str(map_table[first_key.index(second_key[pivot % len(second_key)])][first_key.index(sd)])
+        sd = chr(ord(sd) + (17 if pivot % 2 == 0 else 49))
+        row_idx = map_pivot.index(second_key[pivot % len(second_key)])
+        col_idx = map_pivot.index(sd)
+        cipher_data += str(map_table[row_idx][col_idx])
         pivot += 1
+
+    # chuyển đổi secret number
+    secret_number_convert = ''
+    secret_number = str(secret_number)
+    for sn in secret_number:
+        secret_number_convert += chr(ord(sn) + (17 if secret_number.index(sn) % 2 == 0 else 49))
 
     return '{first_key}{cipher_data}{second_key}{secret_number}' \
         .format(first_key=first_key,
                 cipher_data=cipher_data,
                 second_key=second_key,
-                secret_number=secret_number)
+                secret_number=secret_number_convert)
 
 
 # Giải mã bằng thuật toán Vigenere
@@ -74,23 +97,43 @@ def decode_vigenere(cipher_text):
     first_key = cipher_text[:10]
     cipher_data = cipher_text[10:20]
     second_key = cipher_text[20:30]
-    secret_number = int(cipher_text[30:])
+    secret_number = cipher_text[30:]
+
+    # chuyển đổi secret number
+    secret_number_convert = ''
+    for sn in secret_number:
+        secret_number_convert += chr(ord(sn) - (17 if secret_number.index(sn) % 2 == 0 else 49))
+    secret_number = int(secret_number_convert)
+
+    # data source
+    data_source = []
+    for dt in range(65, 91):
+        data_source.append(chr(dt))
+    for dt in range(97, 123):
+        data_source.append(chr(dt))
+
+    # Tạo map pivot
+    map_pivot = [x for x in first_key]
+    for dt in data_source:
+        if not map_pivot.__contains__(dt):
+            map_pivot.append(dt)
 
     # Tạo bảng ánh xạ
     map_table = []
-    map_pivot = [int(x) for x in first_key]
-    for r in range(0, 10):
-        map_table.append([map_pivot[c] for c in range(0, 10)])
-        map_pivot.append(map_pivot.pop(0))
+    map_pivot_temp = [x for x in map_pivot]
+    for r in range(0, len(data_source)):
+        map_table.append([map_pivot_temp[c] for c in range(0, len(data_source))])
+        map_pivot_temp.append(map_pivot_temp.pop(0))
 
     # Ánh xạ và giải mã
     secret_data = ''
     pivot = 0
     for c in cipher_data:
-        secret_data += first_key[
-            map_table[first_key.index(second_key[pivot % len(second_key)])].index(int(c))]
+        row_idx = map_pivot.index(second_key[pivot % len(second_key)])
+        sd = map_pivot[map_table[row_idx].index(c)]
+        secret_data += chr(ord(sd) - (17 if pivot % 2 == 0 else 49))
         pivot += 1
-        
+
     plain_text = int(secret_data) - secret_number
 
     return plain_text
